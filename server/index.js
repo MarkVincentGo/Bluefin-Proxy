9a4ba8e34316e580783542939a8beb525fde1325
Mark Go <mark.vince.go@gmail.com>
2020-02-07 12:11:15 -0800

    for first deployment

--- server/index.js
+++ server/index.js
@@ -11,6 +11,13 @@ mainApp.use(morgan('dev'));
 mainApp.use(express.static(path.join(__dirname, '../public')));
 
 // Mark Files
+mainApp.get('/bundle.js', (req, res) => {
+  axios.get(`http://localhost:3002/bundle.js`)
+    .then(response => {
+      res.send(response.data)
+    })
+})
+
 mainApp.get('/house', (req, res) => {
   axios.get(`http://localhost:3002/house?listingId=${req.query.listingId}`)
     .then(response => {
@@ -20,15 +27,22 @@ mainApp.get('/house', (req, res) => {
 
 mainApp.post('/schedule', (req, res) => {
   // for testing the front end... no added functionality yet
-  axios.post('http://localhost:3002/schedule', req.body)
+  axios.post('http://52.52.152.21:3002/schedule', req.body)
     .then(response => {
       res.send(response.data);
     });
 });
 
+mainApp.get('/robBundle.js', (req, res) => {
+  axios.get(`http://13.52.171.152:4003/bundle.js`)
+    .then(response => {
+      res.send(response.data)
+    })
+})
+
 mainApp.get('/mortgage:listingId', (req, res) => {
   const { listingId } = req.params;
-  axios.get(`http://localhost:4003/mortgage${listingId}`)
+  axios.get(`http://13.52.171.152:4003/mortgage${listingId}`)
     .then(response => {
       res.send(response.data);
     })
@@ -38,15 +52,22 @@ mainApp.get('/mortgage:listingId', (req, res) => {
 });
 
 mainApp.patch('/adClick', (req, res) => {
-  axios.patch('http:localhost:4003/adClick', req.body)
+  axios.patch('http://13.52.171.152:4003:4003/adClick', req.body)
     .then(response => {
       res.end();
     });
 });
 
+mainApp.get('/kevBundle.js', (req, res) => {
+  axios.get('http://18.144.115.157:4004/bundle.js')
+    .then(response => {
+      res.send(response.data)
+    })
+})
 
-mainApp.get('/listings', (req, res) => {
-  axios.get('http://localhost:4004/listings')
+
+mainApp.get('/similar-listings', (req, res) => {
+  axios.get('http://18.144.115.157:4004/similar-listings')
     .then(response => {
       res.send(response.data);
     });
